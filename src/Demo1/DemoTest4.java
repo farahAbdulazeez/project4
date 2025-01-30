5d6f9e9eedebdea0df862823ddcaed2d50266557
Farah <Farah@DESKTOP-4IKORCG.lan>
2025-01-29 20:06:48 -0600

    practicing GitHub

--- src/Demo1/DemoTest4.java
+++ src/Demo1/DemoTest4.java
@@ -3,6 +3,12 @@ package Demo1;
 public class DemoTest4 {
 	public static void main(String args []) {
 		System.out.println("Java");
+		
+		int a = 10;
+		int b = 15;
+		
+		int result = 10 +15;
+		System.out.println(result);
 	}
 
 }
